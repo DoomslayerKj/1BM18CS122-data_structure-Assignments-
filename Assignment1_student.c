3e8c5b9f328837b814cc5c0e1f46a7f707b25985
VaradVithalKj <47701278+DoomslayerKj@users.noreply.github.com>
2019-08-18 10:15:58 -0700

    Add files via upload

---
+++ Assignment1_student.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+struct
+{int mark,age;
+char name[20];
+}s1;
+clrscr();
+printf("Enter the name : ");
+scanf("%s",s1.name);
+printf("Enter the age and marks : ");
+scanf("%d%d",&s1.age,&s1.mark);
+if((s1.mark >=65 && s1.mark <= 100) && (s1.age>20))
+
+         printf("Student %s qualified for admission",s1.name);
+
+else
+printf("Student %s  NOT qualified for admission",s1.name);
+getch();
+return 0;
+}
